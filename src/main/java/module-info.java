4f6451513e0de12e0449aed0089b713ead0e8d60
Yobi <harisrathor2@gmail.com>
2026-01-13 16:03:13 +0000

    Created the start of the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.enterlinked {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.enterlinked to javafx.fxml;
+    exports com.example.enterlinked;
+}
